627cbeac4b557a070d25cec9d506e98dc983f08a
Siyavush1411 <siyavushmirzaev375@gmail.com>
2025-07-09 00:32:25 +0500

    [Fix] findet worked openai.router token and small fix

--- src/repository/session.py
+++ src/repository/session.py
@@ -1,6 +1,7 @@
+from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./complaints.db"
 
@@ -11,7 +12,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-def get_db() -> Session:
+
+async def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
